12ba9792a2ca81db75d8f1aa0a0167b640c16236
fanrui <1334957263@qq.com>
2019-01-14 14:47:32 +0800

    cloud

---
+++ server/config/src/main/java/com/fanrui/server/config/ConfigServerApplication.java
@@ -0,0 +1,11 @@
+package com.fanrui.server.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+}
