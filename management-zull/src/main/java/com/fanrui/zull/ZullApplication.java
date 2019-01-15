8840dd0dce600c63f19fa1e9d849541b66c1a700
fanrui <1334957263@qq.com>
2019-01-15 16:38:08 +0800

    cloud

--- management-zull/src/main/java/com/fanrui/zull/ZullApplication.java
+++ management-zull/src/main/java/com/fanrui/zull/ZullApplication.java
@@ -3,12 +3,13 @@ package com.fanrui.zull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableHystrix
 @EnableZuulProxy
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ZullApplication {
     public static void main(String[] args) {
