bd5774662044d32af4d3ff8b986363327fd0e3fa
fanrui <1334957263@qq.com>
2019-01-22 16:14:01 +0800

    cloud

---
+++ management-common/src/main/java/com/fanrui/common/config/JpaConfig.java
@@ -0,0 +1,18 @@
+package com.fanrui.common.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement(proxyTargetClass = true)
+@EnableJpaRepositories(basePackages = "com.fanrui.common.repository")
+@EntityScan(basePackages = "com.fanrui.common.entity")
+public class JpaConfig {
+    @Bean
+    PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor(){
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+}
+
