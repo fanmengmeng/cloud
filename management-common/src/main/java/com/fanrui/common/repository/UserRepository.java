f314566c4ea7c16a2c874978613af74498df72ee
fanrui <1334957263@qq.com>
2020-05-25 03:43:54 +0800

    更改springcloud文件夹重命名

--- management-common/src/main/java/com/fanrui/common/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fanrui.common.repository;
-
-import com.fanrui.common.entity.User;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface UserRepository extends ElasticsearchRepository<User,Long> {
-}
